9c3b48f3e10424ec8ce432c0462ea96beeed9363
José Avilés <javiles@externos.gloval.es>
2023-03-14 08:38:27 +0100

    Añado Paises

---
+++ Sales.API/Controllers/CountriesControler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+    {
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesControler : ControllerBase
+        {
+        private readonly DataContext _contex;
+
+        public CountriesControler(DataContext context)
+            {
+             _contex = context;
+            }
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country )
+            {
+                 _contex.Countries.Add(country);
+                await _contex.SaveChangesAsync();   
+                return Ok(country);
+            }
+        }
+    }
